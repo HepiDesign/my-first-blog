84cde6c504ad7af136ea81624bb1fdcad5ed0b8c
HepiDesign <162658380+HepiDesign@users.noreply.github.com>
2024-11-03 20:13:33 -0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')  # Asegúrate de que exista un archivo `home.html` en la carpeta de templates
