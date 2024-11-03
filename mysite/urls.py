894608bf1f0fe36ef6175952e3761e5a7689022e
HepiDesign <162658380+HepiDesign@users.noreply.github.com>
2024-11-03 20:38:23 -0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Agregar include aquí
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),  # Esto enlaza las URLs de la app 'blog'
 ]
